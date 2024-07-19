0f320b212a350919ca4181b2c1e5473636e8d5ec
Acetonerer <bolokan2014@gmail.com>
2024-07-19 15:04:38 +0700

    моя попытка номер 5

--- manage.py
+++ manage.py
@@ -2,11 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,7 +18,22 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
 
-    execute_from_command_line(sys.argv)
+    # Load environment variables from .env file
+    load_dotenv()
+
+    # Handle command line arguments
+    args = sys.argv
+
+    port = os.environ.get('PORT', '8000')
+    execute_from_command_line([sys.argv[0], 'runserver', '0.0.0.0:' + port])
+
+    # Check if the command is either 'makemigrations' or 'migrate'
+    if len(args) > 1 and (args[1] == 'makemigrations' or args[1] == 'migrate'):
+        # Execute the specific command
+        execute_from_command_line(args)
+    else:
+        # Default behavior: execute from command line with original arguments
+        execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
