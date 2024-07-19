858a7049d353613c8551186c23c679fd1556e7bd
Acetonerer <bolokan2014@gmail.com>
2024-07-19 15:01:23 +0700

    моя попытка номер 5

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
 
 
 def main():
@@ -16,7 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    load_dotenv()
+
     execute_from_command_line(sys.argv)
 
 
